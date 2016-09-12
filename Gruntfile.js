e515d6f7e649adcf92e74fd339550312153ffb7c
Anders Bjerner <abjerner@skybrud.dk>
2016-09-12 14:37:35 +0200

    Build stuff

---
+++ Gruntfile.js
@@ -0,0 +1,120 @@
+module.exports = function (grunt) {
+
+    var path = require('path');
+
+	// Load the package JSON file
+	var pkg = grunt.file.readJSON('package.json');
+
+	// get the root path of the project
+	var projectRoot = 'src/' + pkg.name + '/';
+
+	// Load information about the assembly
+	var assembly = grunt.file.readJSON(projectRoot + 'Properties/AssemblyInfo.json');
+
+	// Get the version of the package
+	var version = assembly.informationalVersion ? assembly.informationalVersion : assembly.version;
+
+	var os = require("os");
+	var machineName = os.hostname();
+
+	grunt.initConfig({
+		pkg: pkg,
+		clean: {
+			files: [
+				'releases/temp/**/*.*'
+			]
+		},
+		copy: {
+			binary: {
+				files: [
+					{
+						expand: true,
+						cwd: projectRoot + 'bin/',
+						src: [
+							pkg.name + '.dll',
+							pkg.name + '.xml',
+							'Skybrud.LinkPicker.dll',
+							'Skybrud.LinkPicker.xml',
+							'Skybrud.Umbraco.GridData.dll',
+							'Skybrud.Umbraco.GridData.xml'
+						],
+						dest: 'releases/temp/bin/'
+					}
+				]
+			},
+			resources: {
+			    files: [
+					{
+					    expand: true,
+					    cwd: projectRoot + 'App_Plugins/' + pkg.name + '/',
+					    src: ['**/*.*'],
+					    dest: 'releases/temp/App_Plugins/' + pkg.name + '/'
+					},
+					{
+					    expand: true,
+					    cwd: projectRoot + 'App_Plugins/Skybrud.LinkPicker/',
+					    src: ['**/*.*'],
+					    dest: 'releases/temp/App_Plugins/Skybrud.LinkPicker/'
+					}
+				]
+			},
+			nuget: {
+			    files: [
+					{
+					    expand: true,
+					    cwd: 'releases/nuget/',
+					    src: [pkg.name + '.' + version + '.nupkg'],
+					    dest: 'D:/NuGet/'
+					}
+				]
+			}
+		},
+		nugetpack: {
+			dist: {
+				src: 'src/' + pkg.name + '/' + pkg.name + '.csproj',
+				dest: 'releases/nuget/'
+			}
+		},
+		zip: {
+			release: {
+			    router: function (filepath) {
+			        var name = path.basename(filepath);
+			        return name == 'LICENSE.html' ? name : filepath.replace('releases/temp/', '');
+			    },
+				src: [
+					'releases/temp/**/*.*',
+					projectRoot + '/LICENSE.html'
+				],
+				dest: 'releases/github/' + pkg.name + '.v' + version + '.zip'
+			}
+		},
+		umbracoPackage: {
+		    dist: {
+		        src: 'releases/temp/',
+		        dest: 'releases/umbraco',
+		        options: {
+		            name: pkg.name,
+		            version: version,
+		            url: pkg.url,
+		            license: pkg.license.name,
+		            licenseUrl: pkg.license.url,
+		            author: pkg.author.name,
+		            authorUrl: pkg.author.url,
+		            readme: pkg.readme,
+		            outputName: pkg.name + '.v' + version + '.zip'
+		        }
+		    }
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-nuget');
+	grunt.loadNpmTasks('grunt-zip');
+	grunt.loadNpmTasks('grunt-umbraco-package');
+
+	grunt.registerTask('dev', ['clean', 'copy:binary', 'copy:resources', 'nugetpack', 'zip'/*, 'umbracoPackage'*/]);
+
+	grunt.registerTask('default', ['dev']);
+
+};
--- src/Skybrud.Umbraco.Redirects/Properties/AssemblyInfo.cs
+++ src/Skybrud.Umbraco.Redirects/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Skybrud.Umbraco.Redirects")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Skybrud.Umbraco.Redirects is a package to handle 301 redirects in Umbraco.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Skybrud.dk")]
 [assembly: AssemblyProduct("Skybrud.Umbraco.Redirects")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyInformationalVersion("0.0.1")]
