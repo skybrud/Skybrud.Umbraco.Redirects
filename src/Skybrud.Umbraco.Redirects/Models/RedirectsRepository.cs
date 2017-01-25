e0b75b40405562c30ef722c3e0da3690dab5c2dc
Anders Bjerner <abjerner@skybrud.dk>
2017-01-25 17:29:44 +0100

    Cleanup

--- src/Skybrud.Umbraco.Redirects/Models/RedirectsRepository.cs
+++ src/Skybrud.Umbraco.Redirects/Models/RedirectsRepository.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Linq;
-using System.Linq.Expressions;
-using System.Reflection;
 using Skybrud.LinkPicker;
 using Skybrud.Umbraco.Redirects.Exceptions;
 using Umbraco.Core;
