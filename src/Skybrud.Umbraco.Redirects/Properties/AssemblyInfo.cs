cb9093c3eaf77a6351b1c8adeb4e6999d06d6d3f
Anders Bjerner <abjerner@skybrud.dk>
2017-06-20 18:44:58 +0200

    New release :D

--- src/Skybrud.Umbraco.Redirects/Properties/AssemblyInfo.cs
+++ src/Skybrud.Umbraco.Redirects/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.2.5.7")]
-[assembly: AssemblyInformationalVersion("0.2.7")]
+[assembly: AssemblyVersion("0.2.5.8")]
+[assembly: AssemblyInformationalVersion("0.2.8")]
