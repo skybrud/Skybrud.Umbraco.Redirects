b231f181530536f331e6e6f360a5b374963e4e18
skttl <sk@ecreo.dk>
2019-03-03 21:54:49 +0100

    fix AssemblyInfo

--- src/Skybrud.Umbraco.Redirects/Properties/AssemblyInfo.cs
+++ src/Skybrud.Umbraco.Redirects/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Skybrud.Umbraco.Redirects8")]
+[assembly: AssemblyTitle("Skybrud.Umbraco.Redirects")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Skybrud.Umbraco.Redirects8")]
+[assembly: AssemblyProduct("Skybrud.Umbraco.Redirects")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
