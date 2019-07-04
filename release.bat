68a0f6135a5f68f1fe72e075ef242663bdf1e63d
Anders Bjerner <abjerner@skybrud.dk>
2019-07-04 23:07:44 +0200

    Build stuff

---
+++ release.bat
@@ -0,0 +1,4 @@
+@echo off
+cd src/Skybrud.Umbraco.Redirects
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\msbuild.exe" /t:rebuild /t:pack /p:Configuration=Release /p:PackageOutputPath=../../releases/nuget
+cd ../../
--- src/build/release.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd ../Skybrud.Umbraco.Redirects
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\msbuild.exe" /t:pack /p:Configuration=Release /p:PackageOutputPath=../../releases/nuget
