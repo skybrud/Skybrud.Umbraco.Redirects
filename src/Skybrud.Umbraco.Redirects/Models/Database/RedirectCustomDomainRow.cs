e98e46240918ab64e3da17e11cb5cf96b9a41acf
Anders Bjerner <abjerner@skybrud.dk>
2019-09-19 19:51:12 +0200

    Class is not used for anything at the moment, so might as well outcomment it should the implementation change later

--- src/Skybrud.Umbraco.Redirects/Models/Database/RedirectCustomDomainRow.cs
+++ src/Skybrud.Umbraco.Redirects/Models/Database/RedirectCustomDomainRow.cs
@@ -1,66 +1,66 @@
-﻿using System;
-using NPoco;
-using Umbraco.Core.Persistence.DatabaseAnnotations;
+﻿//using System;
+//using NPoco;
+//using Umbraco.Core.Persistence.DatabaseAnnotations;
 
-namespace Skybrud.Umbraco.Redirects.Models.Database {
+//namespace Skybrud.Umbraco.Redirects.Models.Database {
 
-    /// <summary>
-    /// Class representing the database row of a redirect.
-    /// </summary>
-    [TableName(TableName)]
-    [PrimaryKey(PrimaryKey, AutoIncrement = true)]
-    [ExplicitColumns]
-    public class RedirectCustomDomainRow {
+//    /// <summary>
+//    /// Class representing the database row of a redirect.
+//    /// </summary>
+//    [TableName(TableName)]
+//    [PrimaryKey(PrimaryKey, AutoIncrement = true)]
+//    [ExplicitColumns]
+//    public class RedirectCustomDomainRow {
 
-        #region Constants
+//        #region Constants
 
-        /// <summary>
-        /// Gets the name of the table used in the database.
-        /// </summary>
-        public const string TableName = "SkybrudDomains";
+//        /// <summary>
+//        /// Gets the name of the table used in the database.
+//        /// </summary>
+//        public const string TableName = "SkybrudDomains";
 
-        /// <summary>
-        /// Gets the primary key of the redirects table.
-        /// </summary>
-        public const string PrimaryKey = nameof(Id);
+//        /// <summary>
+//        /// Gets the primary key of the redirects table.
+//        /// </summary>
+//        public const string PrimaryKey = nameof(Id);
 
-        #endregion
+//        #endregion
 
-        #region Properties
+//        #region Properties
 
-        /// <summary>
-        /// Gets or sets the numeric ID (primary key) of the redirect.
-        /// </summary>
-        [Column(PrimaryKey)]
-        [PrimaryKeyColumn(AutoIncrement = true)]
-        public int Id { get; set; }
+//        /// <summary>
+//        /// Gets or sets the numeric ID (primary key) of the redirect.
+//        /// </summary>
+//        [Column(PrimaryKey)]
+//        [PrimaryKeyColumn(AutoIncrement = true)]
+//        public int Id { get; set; }
 
-        /// <summary>
-        /// Gets or sets the unique ID of the redirect.
-        /// </summary>
-        [Column("UniqueId")]
-        public string UniqueId { get; set; }
+//        /// <summary>
+//        /// Gets or sets the unique ID of the redirect.
+//        /// </summary>
+//        [Column("UniqueId")]
+//        public string UniqueId { get; set; }
 
-        /// <summary>
-        /// Gets or sets the root node ID of the redirect.
-        /// </summary>
-        [Column("Domain")]
-        public string Domain { get; set; }
+//        /// <summary>
+//        /// Gets or sets the root node ID of the redirect.
+//        /// </summary>
+//        [Column("Domain")]
+//        public string Domain { get; set; }
         
-        /// <summary>
-        /// Gets or sets the timestamp for when the redirect was created.
-        /// </summary>
-        [Column("Created")]
-        public DateTime Created { get; set; }
+//        /// <summary>
+//        /// Gets or sets the timestamp for when the redirect was created.
+//        /// </summary>
+//        [Column("Created")]
+//        public DateTime Created { get; set; }
 
-        /// <summary>
-        /// Gets or sets the timestamp for when the redirect was last updated.
-        /// </summary>
-        [Column("Updated")]
-        public DateTime Updated { get; set; }
+//        /// <summary>
+//        /// Gets or sets the timestamp for when the redirect was last updated.
+//        /// </summary>
+//        [Column("Updated")]
+//        public DateTime Updated { get; set; }
 
-		#endregion
+//		#endregion
 
-	}
+//	}
 
-}
+//}
