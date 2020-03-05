7ed7b45f11527aa310d62b6690cba926faf77a34
Anders Bjerner <abjerner@skybrud.dk>
2020-03-05 23:06:51 +0100

    Fixed outbound redirect value converter
    The "PropertyValueConverterBase" had some breaking changes in Umbraco 8.1 (which is why the outbound redirect value converter didn't work), so the Umbraco dependency has now been updated to Umbraco 8.1.
    
    (cherry picked from commit a69f46bad6ce869de2a12e99f25b145a6a117dfd)

--- src/Skybrud.Umbraco.Redirects/Models/Outbound/OutboundRedirectPropertyValueConverter.cs
+++ src/Skybrud.Umbraco.Redirects/Models/Outbound/OutboundRedirectPropertyValueConverter.cs
@@ -5,25 +5,25 @@ namespace Skybrud.Umbraco.Redirects.Models.Outbound {
 
     internal class OutboundRedirectPropertyValueConverter : PropertyValueConverterBase {
 
-        public override bool IsConverter(PublishedPropertyType propertyType) {
+        public override bool IsConverter(IPublishedPropertyType propertyType) {
             return propertyType.EditorAlias == "Skybrud.Umbraco.Redirects.OutboundRedirect";
         }
 
-        public override object ConvertSourceToIntermediate(IPublishedElement owner, PublishedPropertyType propertyType, object source, bool preview) {
+        public override object ConvertSourceToIntermediate(IPublishedElement owner, IPublishedPropertyType propertyType, object source, bool preview) {
             return OutboundRedirect.Deserialize(source as string);
         }
 
-        public override object ConvertIntermediateToObject(IPublishedElement owner, PublishedPropertyType propertyType, PropertyCacheLevel referenceCacheLevel, object inter, bool preview) {
+        public override object ConvertIntermediateToObject(IPublishedElement owner, IPublishedPropertyType propertyType, PropertyCacheLevel referenceCacheLevel, object inter, bool preview) {
             return inter as OutboundRedirect;
         }
 
-        public override object ConvertIntermediateToXPath(IPublishedElement owner, PublishedPropertyType propertyType, PropertyCacheLevel referenceCacheLevel, object inter, bool preview) {
+        public override object ConvertIntermediateToXPath(IPublishedElement owner, IPublishedPropertyType propertyType, PropertyCacheLevel referenceCacheLevel, object inter, bool preview) {
             return null;
         }
 
-        public override PropertyCacheLevel GetPropertyCacheLevel(PublishedPropertyType propertyType) => PropertyCacheLevel.None;
+        public override PropertyCacheLevel GetPropertyCacheLevel(IPublishedPropertyType propertyType) => PropertyCacheLevel.None;
 
-        public override System.Type GetPropertyValueType(PublishedPropertyType propertyType) {
+        public override System.Type GetPropertyValueType(IPublishedPropertyType propertyType) {
             return typeof(OutboundRedirect);
         }
     
