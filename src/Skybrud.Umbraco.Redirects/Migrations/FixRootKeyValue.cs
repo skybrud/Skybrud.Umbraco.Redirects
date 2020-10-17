ce3d193510714fa76749805bdfe6033c98fcfc86
Anders Bjerner <abjerner@skybrud.dk>
2020-10-18 00:49:34 +0200

    Rewrote migration to work with SQL CE
    The migration was based on a SQL statement using a nested query, which worked fine on a regular SQL Server, but not so much on SQL CE as nested queries apparently isn't supported. With this commit, the migration will instead fetched all affected rows, and then update each row individually in the database.

--- src/Skybrud.Umbraco.Redirects/Migrations/FixRootKeyValue.cs
+++ src/Skybrud.Umbraco.Redirects/Migrations/FixRootKeyValue.cs
@@ -1,23 +1,48 @@
-﻿using Skybrud.Umbraco.Redirects.Models.Database;
+﻿using System;
+using Skybrud.Umbraco.Redirects.Models.Database;
 using Umbraco.Core.Logging;
 using Umbraco.Core.Migrations;
-using Umbraco.Core.Scoping;
+using Umbraco.Core.Services;
 
 namespace Skybrud.Umbraco.Redirects.Migrations {
     
-    public class FixRootKeyValue : MigrationBase {
+    internal class FixRootKeyValue : MigrationBase {
         
-        private readonly IScopeProvider _scopeProvider;
+        private readonly IContentService _contentService;
 
-        public FixRootKeyValue(IMigrationContext context, IScopeProvider scopeProvider) : base(context) {
-            _scopeProvider = scopeProvider;
+        public FixRootKeyValue(IMigrationContext context, IContentService contentService) : base(context) {
+            _contentService = contentService;
         }
 
         public override void Migrate() {
             
             if (TableExists(RedirectItemSchema.TableName) == false) return;
 
-            Database.Execute("UPDATE SkybrudRedirects SET RootKey = (SELECT uniqueId FROM umbracoNode WHERE id = RootId) WHERE RootId > 0 AND RootKey = '00000000-0000-0000-0000-000000000000';");
+            var rows = Database.Fetch<Row>("SELECT * FROM [SkybrudRedirects] WHERE [RootId] > 0 AND [RootKey] = '00000000-0000-0000-0000-000000000000';");
+            
+            foreach (var row in rows) {
+
+                // Get the content node
+                var content = _contentService.GetById(row.RootId);
+
+                // Write to the log if the root node wasn't found
+                if (content == null) {
+                    Logger.Error<FixRootKeyValue>("Root node with ID {RootId} could not be found for redirect with key {Key}.", row.RootId, row.Key);
+                    continue;
+                }
+
+                // Update the redirect in the database
+                Database.Execute("UPDATE [SkybrudRedirects] SET [RootKey] = @0 WHERE [Key] = @1;", content.Key, row.Key);
+
+            }
+
+        }
+
+        public class Row {
+
+            public Guid Key { get; set; }
+
+            public int RootId { get; set; }
 
         }
 
