346be8c6591235249632bdfe4afb60d1e97741c8
Anders Bjerner <abjerner@skybrud.dk>
2021-07-02 20:37:19 +0200

    Updated middleware etc.
    The middleware is now added through a UmbracoPipelineFilter to work better with Umbraco

--- src/Skybrud.Umbraco.Redirects/Composers/RedirectsComposer.cs
+++ src/Skybrud.Umbraco.Redirects/Composers/RedirectsComposer.cs
@@ -1,12 +1,11 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Skybrud.Umbraco.Redirects.Components;
-using Skybrud.Umbraco.Redirects.Notifications;
+using Skybrud.Umbraco.Redirects.Middleware;
 using Skybrud.Umbraco.Redirects.Services;
-using Skybrud.Umbraco.Redirects.Startup;
 using Umbraco.Cms.Core.Composing;
 using Umbraco.Cms.Core.DependencyInjection;
-using Umbraco.Cms.Core.Notifications;
+using Umbraco.Cms.Web.Common.ApplicationBuilder;
 using Umbraco.Extensions;
 
 namespace Skybrud.Umbraco.Redirects.Composers {
@@ -19,11 +18,22 @@ namespace Skybrud.Umbraco.Redirects.Composers {
             
             builder.Components().Append<MigrationComponent>();
             
+            builder.Services.Configure<UmbracoPipelineOptions>(options => {
+                options.AddFilter(new UmbracoPipelineFilter(
+                    "SkybrudRedirects",
+                    applicationBuilder => { },
+                    applicationBuilder => {
+                        applicationBuilder.UseMiddleware<RedirectsMiddleware>();
+                    },
+                    applicationBuilder => { }
+                ));
+            });
+            
             //builder.AddNotificationHandler<RoutingRequestNotification, RoutingRequestNotificationHandler>();
 
-            builder.Services.Insert(0,
-                new ServiceDescriptor(typeof(IStartupFilter), _ => new RedirectsStartupFilter(),
-                    ServiceLifetime.Transient));
+            //builder.Services.Insert(0,
+            //    new ServiceDescriptor(typeof(IStartupFilter), _ => new RedirectsStartupFilter(),
+            //        ServiceLifetime.Transient));
 
         }
 
--- src/Skybrud.Umbraco.Redirects/Middleware/RedirectsMiddleware.cs
+++ src/Skybrud.Umbraco.Redirects/Middleware/RedirectsMiddleware.cs
@@ -1,27 +1,56 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
 using Skybrud.Umbraco.Redirects.Models;
 using Skybrud.Umbraco.Redirects.Services;
+using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Core.Web;
 
 namespace Skybrud.Umbraco.Redirects.Middleware {
-
+    
     public class RedirectsMiddleware {
-
-        readonly RequestDelegate _next;
+        
+        private readonly RequestDelegate _next;
+        private readonly IUmbracoContextAccessor _umbracoContextAccessor;
         private readonly IRedirectsService _redirectsService;
 
-        public RedirectsMiddleware(RequestDelegate next, IRedirectsService redirectsService) {
+        public RedirectsMiddleware(RequestDelegate next, IUmbracoContextAccessor umbracoContextAccessor, IRedirectsService redirectsService) {
             _next = next;
+            _umbracoContextAccessor = umbracoContextAccessor;
             _redirectsService = redirectsService;
         }
 
-        public async Task Invoke(HttpContext context) {
+        public async Task InvokeAsync(HttpContext context) {
 
+            string pathAndQuery = context.Request.GetEncodedPathAndQuery();
+
+            // Ignore all /umbraco/ requests
+            if (pathAndQuery.IndexOf("/umbraco/", StringComparison.InvariantCultureIgnoreCase) == 0)  {
+                await _next(context);
+                return;
+            }
+            
+            // Get a reference to the published request (may be null)
+            var publishedRequest = _umbracoContextAccessor?.UmbracoContext?.PublishedRequest;
+
+            // If we're inside the Umbraco request pipeline, and an IPublishedContent has been set, we don't do anything
+            if (publishedRequest is not null && publishedRequest.HasPublishedContent()) {
+                await _next(context);
+                return;
+            }
+                
+            // Look for a redirect
             Redirect redirect = _redirectsService.GetRedirectByRequest(context.Request);
 
+            // Carry on like normal if no matching redirects are found
+            if (redirect == null) {
+                await _next(context);
+                return;
+            }
+            
+            // Redirect the user to the destination URL
             context.Response.Redirect(redirect.Destination.Url);
-
-            await _next(context);
 
         }
 
