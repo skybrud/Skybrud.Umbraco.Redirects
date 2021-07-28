2723e3807cd23dbcb70e600329c087a634b6a0cf
Anders Bjerner <abjerner@skybrud.dk>
2021-07-28 19:27:27 +0200

    Removed dashboard registration to C# instead of package.manifest
    Similar to #94 for the Umbraco 8 package.

---
+++ src/Skybrud.Umbraco.Redirects/Dashboards/RedirectsDashboard.cs
@@ -0,0 +1,24 @@
+﻿using Skybrud.Umbraco.Redirects.Helpers;
+using Umbraco.Cms.Core.Dashboards;
+
+namespace Skybrud.Umbraco.Redirects.Dashboards {
+    
+    public class RedirectsDashboard : IDashboard {
+        
+        private readonly RedirectsBackOfficeHelper _backoffice;
+        
+        public string Alias => "Skybrud.Umbraco.Redirects";
+
+        public string[] Sections => new[] { "content" };
+
+        public string View => $"/App_Plugins/Skybrud.Umbraco.Redirects/Views/Dashboard.html?v={_backoffice.GetCacheBuster()}";
+
+        public IAccessRule[] AccessRules => _backoffice.GetDashboardAccessRules();
+
+        public RedirectsDashboard(RedirectsBackOfficeHelper backoffice) {
+            _backoffice = backoffice;
+        }
+
+    }
+
+}
