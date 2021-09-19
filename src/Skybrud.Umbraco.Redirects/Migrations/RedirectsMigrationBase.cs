46d9a30692b9b2030d6e5c387a4ea7e60312af9b
Anders Bjerner <abjerner@skybrud.dk>
2021-09-19 20:11:59 +0200

    Updated migration with better approach for dropping column
    Fixes #106
    
    Older versions of SQL Server appear to not support the "IF EXISTS" syntax used when attempting to drop the IsRegex column from the redirects table, resulting in issues when installing this package. With this commit, the migration uses Umbraco's logic to first check whether the column does in fact exist, and the migration will drop the column if it does.

---
+++ src/Skybrud.Umbraco.Redirects/Migrations/RedirectsMigrationBase.cs
@@ -0,0 +1,19 @@
+﻿using Umbraco.Cms.Infrastructure.Migrations;
+
+namespace Skybrud.Umbraco.Redirects.Migrations {
+    
+    public abstract class RedirectsMigrationBase : MigrationBase {
+        
+        protected RedirectsMigrationBase(IMigrationContext context) : base(context) { }
+        
+        protected void DropColumn(string tableName, string columnName) {
+            Database.Execute(string.Format(SqlSyntax.DropColumn, SqlSyntax.GetQuotedTableName(tableName), SqlSyntax.GetQuotedColumnName(columnName)));
+        }
+        
+        protected void DropColumnIfExists(string tableName, string columnName) {
+            if (ColumnExists(tableName, columnName)) DropColumn(tableName, columnName);
+        }
+
+    }
+
+}
--- src/Skybrud.Umbraco.Redirects/Migrations/RemoveIsRegexColumnMigration.cs
+++ src/Skybrud.Umbraco.Redirects/Migrations/RemoveIsRegexColumnMigration.cs
@@ -1,15 +1,14 @@
-﻿using Skybrud.Umbraco.Redirects.Models.Dtos;
-using Skybrud.Umbraco.Redirects.Models.Schema;
+﻿using Skybrud.Umbraco.Redirects.Models.Schema;
 using Umbraco.Cms.Infrastructure.Migrations;
 
 namespace Skybrud.Umbraco.Redirects.Migrations {
 
-    public class RemoveIsRegexColumnMigration : MigrationBase {
+    public class RemoveIsRegexColumnMigration : RedirectsMigrationBase {
 
         public RemoveIsRegexColumnMigration(IMigrationContext context) : base(context) { }
 
         protected override void Migrate() {
-            Database.Execute($"ALTER TABLE {RedirectSchema.TableName} DROP COLUMN IF EXISTS {nameof(RedirectSchema.IsRegex)};");
+            DropColumnIfExists(RedirectSchema.TableName, nameof(RedirectSchema.IsRegex));
         }
 
     }
