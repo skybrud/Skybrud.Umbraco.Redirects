c699a4adfe25a23f478eafbee56586bfbfa76db9
Anders Bjerner <abjerner@skybrud.dk>
2021-12-09 23:36:43 +0100

    Updated how the package is configured during installation
    If the redirects package was installed before the upgrade installer was finished, two exceptions would occur:
    
    - Migration to install the database would fail as the database isn't during before the Umbraco installer has been completed.
    - Middleware would also run when trying to access the Umbraco installer, thus failing as the middleware would try to access the database through the redirects service
    
    With this commit, the migration plan for the redirects package will only be set up when the runtime level is "Run".
    
    The middleware will now not run when the runtime level is "Install", but will still run when the runtime level is "Upgrade". This might need a bit more testing, but seems to be fine as there *is* a database at this level.

--- src/Skybrud.Umbraco.Redirects/Middleware/RedirectsMiddleware.cs
+++ src/Skybrud.Umbraco.Redirects/Middleware/RedirectsMiddleware.cs
@@ -4,25 +4,36 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Extensions;
 using Skybrud.Umbraco.Redirects.Models;
 using Skybrud.Umbraco.Redirects.Services;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Services;
 
 namespace Skybrud.Umbraco.Redirects.Middleware {
     
     public class RedirectsMiddleware {
         
         private readonly RequestDelegate _next;
+        private readonly IRuntimeState _runtimeState;
         private readonly IRedirectsService _redirectsService;
 
-        public RedirectsMiddleware(RequestDelegate next, IRedirectsService redirectsService) {
+        public RedirectsMiddleware(RequestDelegate next, IRuntimeState runtimeState, IRedirectsService redirectsService) {
             _next = next;
+            _runtimeState = runtimeState;
             _redirectsService = redirectsService;
         }
 
         public async Task InvokeAsync(HttpContext context) {
 
+            // If Umbraco hasn't been installed yet, the middleware shouldn't do anything (interacting with the
+            // redirects service will fail as the database isn't setup yet)
+            if (_runtimeState.Level == RuntimeLevel.Install) {
+                await _next(context);
+                return;
+            }
+
             string pathAndQuery = context.Request.GetEncodedPathAndQuery();
 
             // Ignore all /umbraco/ requests
-            if (pathAndQuery.IndexOf("/umbraco/", StringComparison.InvariantCultureIgnoreCase) == 0)  {
+            if (pathAndQuery.IndexOf("/umbraco/", StringComparison.InvariantCultureIgnoreCase) == 0) {
                 await _next(context);
                 return;
             }
