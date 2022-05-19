26ad49e362a0a5c53cb70de6216abac863e19cf0
Anders Bjerner <abjerner@skybrud.dk>
2022-05-19 23:13:53 +0200

    Added "RedirectsPackage" class with various information about the package
    A similar class already exists in the V9 package.

---
+++ src/Skybrud.Umbraco.Redirects/RedirectsPackage.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Semver;
+using Skybrud.Essentials.Reflection;
+
+namespace Skybrud.Umbraco.Redirects {
+
+    /// <summary>
+    /// Static class with various information and constants about the package.
+    /// </summary>
+    public static class RedirectsPackage {
+
+        /// <summary>
+        /// Gets the alias of the package.
+        /// </summary>
+        public const string Alias = "Skybrud.Umbraco.Redirects";
+
+        /// <summary>
+        /// Gets the friendly name of the package.
+        /// </summary>
+        public const string Name = "Skybrud Redirects";
+
+        /// <summary>
+        /// Gets the version of the package.
+        /// </summary>
+        public static readonly Version Version = typeof(RedirectsPackage).Assembly.GetName().Version;
+
+        /// <summary>
+        /// Gets the semantic version of the package.
+        /// </summary>
+        public static readonly SemVersion SemVersion = SemVersion.Parse(ReflectionUtils.GetInformationalVersion(typeof(RedirectsUtils).Assembly));
+
+        /// <summary>
+        /// Gets the URL of the GitHub repository for this package.
+        /// </summary>
+        public const string GitHubUrl = "https://github.com/skybrud/Skybrud.Umbraco.Redirects";
+
+        /// <summary>
+        /// Gets the URL of the issue tracker for this package.
+        /// </summary>
+        public const string IssuesUrl = "https://github.com/skybrud/Skybrud.Umbraco.Redirects/issues";
+
+        /// <summary>
+        /// Gets the URL of the documentation for this package.
+        /// </summary>
+        public const string DocumentationUrl = "https://packages.skybrud.dk/skybrud.umbraco.redirects/";
+
+    }
+
+}
