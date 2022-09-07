8dc5473cca19a60a406f971b5a8c774dcb53cd4a
Anders Bjerner <abjerner@skybrud.dk>
2022-09-07 19:13:43 +0200

    Implemented dedicated SaveBackup methods in the internal RedirectsUtils class
    + updated AddDestinationColumnsMigration class to use new method
    
    See #149

--- src/Skybrud.Umbraco.Redirects/Migrations/AddDestinationColumnsMigration.cs
+++ src/Skybrud.Umbraco.Redirects/Migrations/AddDestinationColumnsMigration.cs
@@ -1,20 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json.Linq;
-using Skybrud.Essentials.Json;
 using Skybrud.Umbraco.Redirects.Models;
 using Skybrud.Umbraco.Redirects.Models.Dtos;
 using Umbraco.Cms.Infrastructure.Migrations;
-using Umbraco.Extensions;
 
 namespace Skybrud.Umbraco.Redirects.Migrations {
-    
+
     internal class AddDestinationColumnsMigration : MigrationBase {
-        
+
         private readonly IWebHostEnvironment _webHostEnvironment;
 
         public AddDestinationColumnsMigration(IMigrationContext context, IWebHostEnvironment webHostEnvironment) : base(context) {
@@ -25,16 +21,8 @@ namespace Skybrud.Umbraco.Redirects.Migrations {
 
             try {
 
-                // Get the DTOs for all redirects in the database
-                List<RedirectDto> dtos = Database.Fetch<RedirectDto>("SELECT * FROM [SkybrudRedirects];");
-
-                // Map the path to a special redirects folder
-                string dir = _webHostEnvironment.MapPathWebRoot("~/App_Data/Skybrud.Umbraco.Redirects");
-                if (!Directory.Exists(dir)) Directory.CreateDirectory(dir);
-
-                // Map the path to the JSON file and save the DTOs to disk as a backup
-                string path = Path.Combine(dir, $"Redirects_Backup_{DateTime.UtcNow:yyyyMMddHHmmss}.json");
-                JsonUtils.SaveJsonArray(path, JArray.FromObject(dtos));
+                // Save a backup of all redirects
+                var result = RedirectsUtils.SaveBackup(_webHostEnvironment, Database);
 
                 // Add the "DestinationQuery" column to the database table
                 if (!ColumnExists(RedirectDto.TableName, nameof(RedirectDto.DestinationQuery))) {
@@ -45,7 +33,7 @@ namespace Skybrud.Umbraco.Redirects.Migrations {
                         .WithDefaultValue(string.Empty)
                         .Do();
                 }
-                
+
                 // Add the "DestinationFragment" column to the database table
                 if (!ColumnExists(RedirectDto.TableName, nameof(RedirectDto.DestinationFragment))) {
                     Create
@@ -55,20 +43,20 @@ namespace Skybrud.Umbraco.Redirects.Migrations {
                         .WithDefaultValue(string.Empty)
                         .Do();
                 }
-                
+
                 // Wrap the DTOs as RedirectItem instances
-                List<Redirect> redirects = dtos
+                List<Redirect> redirects = result.Dtos
                     .Select(x => new Redirect(x))
                     .ToList();
-                
+
                 foreach (Redirect redirect in redirects) {
-                    
+
                     // Skip the redirect if we didn't detect any changes
                     if (!RedirectsUtils.NormalizeUrlParts(redirect)) {
                         Logger.LogInformation("Redirect with key {Key} is already up-to-date.", redirect.Key);
                         continue;
                     }
-                    
+
                     // Update the redirect in the database
                     Database.Execute(
                         "UPDATE [SkybrudRedirects] SET [DestinationUrl] = @0, [DestinationQuery] = @1, [DestinationFragment] = @2 WHERE [Key] = @3;",
@@ -76,13 +64,13 @@ namespace Skybrud.Umbraco.Redirects.Migrations {
                     );
 
                     Logger.LogInformation("Updated redirect with key {Key}.", redirect.Key);
-                    
+
                 }
 
             } catch (Exception ex) {
-            
+
                 throw new Exception("Failed migration for Skybrud.Umbraco.Redirects. See the Umbraco log for more information.", ex);
-            
+
             }
 
         }
--- src/Skybrud.Umbraco.Redirects/RedirectsUtils.cs
+++ src/Skybrud.Umbraco.Redirects/RedirectsUtils.cs
@@ -1,7 +1,19 @@
-﻿using Skybrud.Essentials.Reflection;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Newtonsoft.Json.Linq;
+using Skybrud.Essentials.Json;
+using Skybrud.Essentials.Reflection;
 using Skybrud.Essentials.Strings.Extensions;
 using Skybrud.Umbraco.Redirects.Models;
+using Skybrud.Umbraco.Redirects.Models.Dtos;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Extensions;
 using Umbraco.Cms.Core.Semver;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Web.Common.DependencyInjection;
+using Umbraco.Extensions;
 
 namespace Skybrud.Umbraco.Redirects {
 
@@ -87,8 +99,71 @@ namespace Skybrud.Umbraco.Redirects {
 
         }
 
+        public class RedirectsBackupResult {
+
+            public string Path { get; }
+
+            public IReadOnlyList<RedirectDto> Dtos { get; }
+
+            public RedirectsBackupResult(string path, IReadOnlyList<RedirectDto> dtos) {
+                Path = path;
+                Dtos = dtos;
+            }
+
+        }
+
         public static SemVersion GetSemVersion() {
             return SemVersion.Parse(ReflectionUtils.GetInformationalVersion(typeof(RedirectsUtils).Assembly));
+        }
+
+        /// <summary>
+        /// Saves a backup of all redirects to a new JSON file to the <c>~/umbraco/Data/Skybrud.Umbraco.Redirects</c>
+        /// directory. If there are currently not redirects in the database, no redirects will be saved.
+        /// </summary>
+        public static void SaveBackup() {
+            var webHostEnvironment = GetService<IWebHostEnvironment>();
+            var database = GetService<IUmbracoDatabase>();
+            SaveBackup(webHostEnvironment, database);
+        }
+
+        /// <summary>
+        /// Saves a backup of all redirects to a new JSON file to the <c>~/umbraco/Data/Skybrud.Umbraco.Redirects</c>
+        /// directory. If there are currently not redirects in the database, no redirects will be saved.
+        /// </summary>
+        /// <param name="webHostEnvironment">A reference to the current <see cref="IWebHostEnvironment"/>.</param>
+        /// <param name="database">A reference to the current <see cref="IUmbracoDatabase"/>.</param>
+        /// <returns>An instance of <see cref="RedirectsBackupResult"/>.</returns>
+        public static RedirectsBackupResult SaveBackup(IWebHostEnvironment webHostEnvironment, IUmbracoDatabase database) {
+
+            // Map the path to a special redirects folder (both old and new)
+            string dir1 = webHostEnvironment.MapPathWebRoot($"~/App_Data/{RedirectsPackage.Alias}");
+            string dir2 = webHostEnvironment.MapPathContentRoot($"{Constants.SystemDirectories.Data}/{RedirectsPackage.Alias}");
+
+            // Older releases would create a bacup in the "AppData" directory in the web root. Even though the
+            // "App_Data" still appear to be protected in ASP.NET Core, we shouldn't save data to the web root. So
+            // we've previously created a folder inside "App_Data" in the web root, we move the directory to the
+            // correct location.
+            if (Directory.Exists(dir1) && !Directory.Exists(dir2)) Directory.Move(dir1, dir2);
+
+            // Create a new directory if it doesn't already exist
+            if (!Directory.Exists(dir2)) Directory.CreateDirectory(dir2);
+
+            // Map the path to the JSON file
+            string path = Path.Combine(dir2, $"Redirects_Backup_{DateTime.UtcNow:yyyyMMddHHmmss}.json");
+
+            // Get the DTOs for all redirects in the database
+            List<RedirectDto> dtos = database.Fetch<RedirectDto>("SELECT * FROM [SkybrudRedirects];");
+
+            // Save the JSON file to the disk
+            JsonUtils.SaveJsonArray(path, JArray.FromObject(dtos));
+
+            // Return the result
+            return new RedirectsBackupResult(path, dtos);
+
+        }
+
+        internal static T GetService<T>()  {
+            return (T) StaticServiceProvider.Instance.GetService(typeof(T));
         }
 
     }
