357bb325fba831a88a51f7a8358dad96c739a166
Anders Bjerner <abjerner@skybrud.dk>
2022-12-01 00:01:12 +0100

    Fixed content app JavaScript error

--- src/Skybrud.Umbraco.Redirects/wwwroot/Scripts/Controllers/ContentApp.js
+++ src/Skybrud.Umbraco.Redirects/wwwroot/Scripts/Controllers/ContentApp.js
@@ -1,4 +1,4 @@
-﻿angular.module("umbraco").controller("Skybrud.Umbraco.Redirects.ContentApp.Controller", function($scope, $routeParams, editorState) {
+﻿angular.module("umbraco").controller("Skybrud.Umbraco.Redirects.ContentApp.Controller", function ($scope, $routeParams, editorState) {
 
     // Declare the view model
     const vm = this;
@@ -8,11 +8,19 @@
         id: editorState.current.id,
         key: editorState.current.key,
         name: editorState.current.variants ? editorState.current.variants[0].name : editorState.current.name,
-        url: editorState.current.mediaLink ? editorState.current.mediaLink : editorState.current.urls[0].text,
-        published: !!editorState.current.mediaLink || editorState.current.urls[0].isUrl,
+        url: null,
+        published: false,
         trashed: editorState.current.trashed,
         type: editorState.current.udi.indexOf("umb://media/") === 0 ? "media" : "content",
         mode: $routeParams.create ? "create" : "list"
     };
+
+    if (vm.node.type === "media") {
+        vm.node.published = editorState.current.id > 0;
+        vm.node.url = editorState.current.mediaLink;
+    } else if (editorState.current.urls && editorState.current.urls.length > 0) {
+        vm.node.published = editorState.current.urls[0].isUrl;
+        vm.node.url = editorState.current.urls[0].text;
+    }
 
 });
