48fa436d42739d6a0a4d1c5599d68cfbbc8b721c
Anders Bjerner <abjerner@skybrud.dk>
2023-05-09 15:25:05 +0200

    And some XML documentation

--- src/Skybrud.Umbraco.Redirects/Config/RedirectsDashboardSettings.cs
+++ src/Skybrud.Umbraco.Redirects/Config/RedirectsDashboardSettings.cs
@@ -1,8 +1,14 @@
 ﻿namespace Skybrud.Umbraco.Redirects.Config {
 
+    /// <summary>
+    /// Class representing the settings for the global redirects dashboard.
+    /// </summary>
     public class RedirectsDashboardSettings {
 
-        public bool Enabled { get; set; } = true;
+        /// <summary>
+        /// Gets whether the dashboard should be enabled. Default is <see langword="true"/>.
+        /// </summary>
+        public bool Enabled => true;
 
     }
 
