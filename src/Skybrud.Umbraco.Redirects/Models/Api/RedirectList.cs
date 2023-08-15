70c7699163362a43f4eecf3c46fc148cc4c1656c
Anders Bjerner <abjerner@skybrud.dk>
2023-08-15 22:14:15 +0200

    Introduced new "RedirectList" to replace anonymous class
    #179 adds JSON attributes to properties in the "RedirectsSearchResultPagination" class, but this class isn't returned directly in the API. The existing implementation would return an anonymous class, which we can't add attributes to, so this commit introduces a "RedirectList" to be returned instead.

---
+++ src/Skybrud.Umbraco.Redirects/Models/Api/RedirectList.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+#pragma warning disable CS1591
+
+namespace Skybrud.Umbraco.Redirects.Models.Api {
+
+    public class RedirectList {
+
+        [JsonProperty("pagination")]
+        [JsonPropertyName("pagination")]
+        public RedirectsSearchResultPagination Pagination { get; }
+
+        [JsonProperty("items")]
+        [JsonPropertyName("items")]
+        public IEnumerable<RedirectModel> Items { get; }
+
+        public RedirectList(RedirectsSearchResultPagination pagination, IEnumerable<RedirectModel> items) {
+            Pagination = pagination;
+            Items = items;
+        }
+
+    }
+
+}
