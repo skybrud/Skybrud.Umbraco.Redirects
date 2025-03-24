96b725974f334a20cc1cf519c5451958904cba65
Anders Bjerner <abjerner@skybrud.dk>
2025-03-24 22:04:24 +0100

    Added misc extension methods for configuring the package

--- src/Skybrud.Umbraco.Redirects/Extensions/RedirectsExtensions.cs
+++ src/Skybrud.Umbraco.Redirects/Extensions/RedirectsExtensions.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Skybrud.Umbraco.Redirects.Config;
 using Skybrud.Umbraco.Redirects.Middleware;
 
 namespace Skybrud.Umbraco.Redirects.Extensions;
@@ -9,6 +12,46 @@ namespace Skybrud.Umbraco.Redirects.Extensions;
 /// Static class with various extension methods used throughout the package.
 /// </summary>
 public static class RedirectsExtensions {
+
+    /// <summary>
+    /// Registers a class used to configure the redirects settings. Note: These are run before all <see cref="PostConfigureRedirects"/>.
+    /// </summary>
+    /// <typeparam name="T">The class used for configuring the options.</typeparam>
+    /// <param name="services">The <see cref="IServiceCollection"/> to add the services to.</param>
+    /// <returns>The <see cref="IServiceCollection"/>> so that additional calls can be chained.</returns>
+    public static IServiceCollection ConfigureRedirects<T>(this IServiceCollection services) where T : class, IConfigureOptions<RedirectsSettings> {
+        return services.AddSingleton<IConfigureOptions<RedirectsSettings>, T>();
+    }
+
+    /// <summary>
+    /// Registers an action used to configure the redirects settings. Note: These are run before all <see cref="PostConfigureRedirects"/>.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/> to add the services to.</param>
+    /// <param name="configureOptions">The action used to configure the options.</param>
+    /// <returns>The <see cref="IServiceCollection"/>> so that additional calls can be chained.</returns>
+    public static IServiceCollection ConfigureRedirects(this IServiceCollection services, Action<RedirectsSettings> configureOptions) {
+        return services.Configure(configureOptions);
+    }
+
+    /// <summary>
+    /// Registers a class used to configure the redirects settings. Note: These are run after all <see cref="ConfigureRedirects"/>.
+    /// </summary>
+    /// <typeparam name="T">The class used for configuring the options.</typeparam>
+    /// <param name="services">The <see cref="IServiceCollection"/> to add the services to.</param>
+    /// <returns>The <see cref="IServiceCollection"/>> so that additional calls can be chained.</returns>
+    public static IServiceCollection PostConfigureRedirects<T>(this IServiceCollection services) where T : class, IPostConfigureOptions<RedirectsSettings> {
+        return services.AddSingleton<IPostConfigureOptions<RedirectsSettings>, T>();
+    }
+
+    /// <summary>
+    /// Registers an action used to configure the redirects settings. Note: These are run after all <see cref="ConfigureRedirects"/>.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/> to add the services to.</param>
+    /// <param name="configureOptions">The action used to configure the options.</param>
+    /// <returns>The <see cref="IServiceCollection"/>> so that additional calls can be chained.</returns>
+    public static IServiceCollection PostConfigureRedirects(this IServiceCollection services, Action<RedirectsSettings> configureOptions) {
+        return services.PostConfigure(configureOptions);
+    }
 
     /// <summary>
     /// Returns the <see cref="Uri"/> of the specified <paramref name="request"/>.
