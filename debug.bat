d36f6fbad717af12fc1bb959c774c2606ad4331d
Anders Bjerner <abj@limbo.works>
2026-07-14 13:53:12 +0200

    Updated the package to target Umbraco 18

--- debug.bat
+++ debug.bat
@@ -1,2 +1,2 @@
 @echo off
-dotnet build src/Skybrud.Umbraco.Redirects --configuration Debug /t:rebuild /t:pack -p:PackageOutputPath=c:\nuget\Umbraco17
+dotnet build src/Skybrud.Umbraco.Redirects --configuration Debug /t:rebuild /t:pack -p:PackageOutputPath=c:\nuget\Umbraco18
