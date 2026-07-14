c6736e64fcd2c8ee94ff29389f8f533bff2b78d5
Anders Bjerner <abj@limbo.works>
2026-07-14 14:09:10 +0200

    Changed imported namespace due to Skybrud.Essentials update

--- src/Skybrud.Umbraco.Redirects/Services/RedirectsService.cs
+++ src/Skybrud.Umbraco.Redirects/Services/RedirectsService.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Web;
 using Microsoft.AspNetCore.Http;
 using NPoco;
-using Skybrud.Essentials.Collections.Extensions;
+using Skybrud.Essentials.Collections.Enumerables.Extensions;
 using Skybrud.Essentials.Common;
 using Skybrud.Essentials.Strings.Extensions;
 using Skybrud.Umbraco.Redirects.Exceptions;
